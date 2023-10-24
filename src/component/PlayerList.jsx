9f5de0d8efa921ab188bf1ee996d203e57868edd
Oyindamola <oyindamolababalola8@gmail.com>
2023-10-24 14:54:38 +0100

    my commit

---
+++ src/component/PlayerList.jsx
@@ -0,0 +1,22 @@
+import './PlayerList.css'
+const Players = ({name, image, team, nationality, age,jerseyNumber}) => {
+  return (
+    <div className="container">
+    <div className="card">
+      <img src={image} alt="picture" />
+      <div>
+        <h3 className="title">Name:{name}</h3>
+        <p>Age:{age}</p>
+        <p>Nationality:{nationality}</p>
+        </div>
+        <div>
+        <p>Team:{team}</p>
+        <p>Age:{age}</p>
+        <p>Jersey#:{JerseyNumber}</p>
+      </div>
+      </div>
+  </div>
+  );
+};
+
+export default Players;
